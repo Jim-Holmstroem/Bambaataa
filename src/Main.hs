afc3cf8e23c9da1f158826a550264600df5d72a7
Jim-Holmstroem <jim.holmstroem@gmail.com>
2016-05-13 21:22:40 +0000

    A bit more progress

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,16 @@
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Data.Hashable
+import qualified Data.ByteString.Char8 as BSC8
+import qualified Data.ByteString.Lazy.Char8 as BSLC8
 import Control.Monad.IO.Class
 
 import Database.Redis
 import Snap
 
--- TODO utf8
-
+-- TODO does it handle utf8?
+-- FIXME Unable to add a jobb when the queue is empty and a worker is requesting a Job
 
 main :: IO ()
 main = do
@@ -18,9 +20,38 @@ main = do
 
 site :: Connection -> Snap ()
 site conn =
-    route [ ("set/:key/:value", setHandler conn)
-          , ("get/:key", getHandler conn)
+    route [ ("jobs", method POST $ addJob conn)
+          , ("jobs/:jobId", method GET $ getJob conn)
+          , ("requestJob", method GET $ requestJob conn)
           ]
+
+
+getJob :: Connection -> Snap ()
+getJob conn = do
+    Just jobId <- getParam "jobId"
+    response <- liftIO $ runRedis conn $ hget "job:" jobId
+    case response of (Right (Just input)) -> writeBS input
+
+
+requestJob :: Connection -> Snap ()
+requestJob conn = do
+    response <- liftIO $ runRedis conn $ brpoplpush "notcompleted" "inprogress" 60 -- TODO handle timeout properly
+    case response of (Right (Just jobId)) -> writeBS jobId
+
+addJob :: Connection -> Snap ()
+addJob conn = do
+    input <- readRequestBody (256 * 2^20)
+    let jobId = BSC8.pack $ show $ hash input
+    response <- liftIO $ runRedis conn $ multiExec $ do -- TODO handling if this fails
+        hmset ("job:") [ ("input", "poop")
+                       , ("n_retries", "0")
+                       ]
+        lpush "notcompleted" [jobId] -- TODO handling duplicate hash
+
+    writeBS $ "ok"
+
+
+
 
 setHandler :: Connection -> Snap ()
 setHandler conn = do
